67a23890d8e5301f8887e1a6a1194e30050adf60
SprihaDeshpande <50254090+SprihaDeshpande@users.noreply.github.com>
2019-10-17 20:41:43 -0700

    Shipt_UIAutomation Task

---
+++ Shipt_UIAutomation/features/account.feature
@@ -0,0 +1,7 @@
+@account-page
+Feature: Test Account Feature on Shipt
+Scenario: Test Shipt Account Page
+  Given I navigate to "Shipt"
+  And I wait for 2 seconds
+  And I enter good credentials on to login
+  And I click "login button"
---
+++ Shipt_UIAutomation/features/step_definitions/stepdefs.rb
@@ -0,0 +1,17 @@
+Given(/^I navigate to "([^"]*)"$/) do |term|
+@browser ||= Watir::Browser.new :Chrome
+@browser.goto "https://staging-shop.shipt.com/"
+end
+
+When /^I wait for (\d+) seconds?$/ do |n|
+    sleep(n.to_i)
+end
+
+When /^I enter good credentials on to login?$/ do
+    @browser.text_field(:xpath => '//input[@data-test="LoginForm-emailField"]').set "spriha@shipt.com"
+    @browser.text_field(:xpath => '//input[@type="password"]').set("Spriha@123")
+end
+
+When /^I click "([^"]*)"$/ do |button_name|
+@browser.button.click
+end
